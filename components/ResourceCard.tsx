a57bca6eec12fb1b1ab1b6b4dbc117e2301826fe
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-11-05 10:48:45 +0100

    Some clean up
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -37,7 +37,7 @@ const ResourceCard = ({ id, title, image, downloadNumber, downloadLink }: Props)
         </div>
         <Link href={downloadLink} target="_blank" className="flex-center text-gradient_purple-blue body-semibold gap-1.5">
           Download Now
-          <Image src="/arrow-blue.svg" width={13} height={10} alt="arrow" />
+          <Image src="/arrow-blue.svg" width={13} height={10} alt="" />
         </Link>
       </CardContent>
     </Card>
