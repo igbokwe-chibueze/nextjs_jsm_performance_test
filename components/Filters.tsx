77ab7b31b410628afb283fa7cb8c6876f42bb6d3
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-11-05 04:59:52 +0100

    Implemented ResourceCard,  Header and Filtering, Search functionality and ResourcePlayList.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,15 +1,38 @@
 "use client"
 
+import { formUrlQuery } from '@/sanity/utils';
 import { useState } from 'react'
+import { useSearchParams, useRouter } from 'next/navigation'
 
 const links = ['all', 'Next 13', 'frontend', 'backend', 'fullstack']
 
 const Filters = () => {
-    const [active, setActive] = useState('');
+  const [active, setActive] = useState('');
+  const searchParms = useSearchParams();
+  const router = useRouter();
 
-    const handleFilter = (link: string) => {
-        setActive(link);
+  const handleFilter = (link: string) => {
+    let newUrl = '';
+    
+    if(active === link) {
+      setActive('');
+
+      newUrl = formUrlQuery({
+        params: searchParms.toString(),
+        keysToRemove: ['category'],
+      })
+    } else {
+      setActive(link);
+
+      newUrl = formUrlQuery({
+        params: searchParms.toString(),
+        key: 'category',
+        value: link.toLowerCase(),
+      })
     }
+    
+    router.push(newUrl, { scroll: false });
+  }
 
   return (
     <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
