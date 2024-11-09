c33be8a840ac3f4f99bb922ffa13c1e487b0a119
igbokwe-chibueze <igbokwechibueze@gmail.com>
2024-11-09 15:53:58 +0100

    cleaning
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -19,7 +19,7 @@ export const revalidate = 300;
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
         <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
-          <h1 className="sm:heading1 heading2 mb-6 text-center text-white">JavaScript Mastery Resources</h1>
+          <h1 className="sm:heading1 heading2 mb-6 text-center text-white">JavaScript Mastery Resources Performance Test</h1>
         </div>
         <p>New Main</p>
         <SearchForm />
